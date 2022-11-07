b3f4e1bcfae75528d2e3a1e2ca8b40c0c91309ea
C1325737 Pedro Paulo dos Santos Soares <c1325737@interno.bb.com.br>
2022-11-07 19:32:19 -0300

    Initial Commit

---
+++ facebookLogin/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  facebookLogin
+//
+//  Created by Pedro Soares on 07/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ facebookLogin/facebookLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  facebookLoginApp.swift
+//  facebookLogin
+//
+//  Created by Pedro Soares on 07/11/22.
+//
+
+import SwiftUI
+
+@main
+struct facebookLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
