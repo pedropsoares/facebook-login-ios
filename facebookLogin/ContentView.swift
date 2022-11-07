cce9212179f1246d5bef0623694dc641581c88a5
pedropsoares <pedropaulos.1998@gmial.com>
2022-11-07 19:36:37 -0300

    finished implementations

--- facebookLogin/ContentView.swift
+++ facebookLogin/ContentView.swift
@@ -8,19 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	
+	@State var email: String = ""
+	@State var password: String = ""
+	
+	var body: some View {
+		ZStack {
+			
+			Color("BackgroundColor").ignoresSafeArea()
+			
+			VStack {
+				Image("logo")
+					.resizable()
+					.aspectRatio(contentMode: .fit)
+					.frame(width: 280)
+				
+				Spacer().frame(height: 64)
+				
+				VStack {
+					TextField("Email", text: $email)
+						.padding(.top, 24)
+						.padding(.bottom, 12)
+						.padding(.horizontal)
+						.keyboardType(.emailAddress)
+					Divider()
+					SecureField("Password", text: $password)
+						.padding(.top, 12)
+						.padding(.bottom, 24)
+						.padding(.horizontal)
+				}.background(Color.white)
+					.cornerRadius(4.0)
+				
+				Spacer().frame(height: 16)
+				
+				Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+					Text("Login")
+						.font(.title3)
+						.bold()
+						.foregroundColor(.white)
+						.padding(20)
+						.frame(maxWidth: .infinity)
+						.background(Color("ButtonColor"))
+						.cornerRadius(4.0)
+				}
+				
+				Spacer().frame(height: 64)
+				
+				VStack(spacing: 8) {
+					Button(action: {}) {
+						Text("Don't have an account? Sign up.")
+							.foregroundColor(.white)
+							.bold()
+					}
+					
+					Button(action: {}) {
+						Text("Forgot your password?")
+							.foregroundColor(Color("TextColor"))
+					}
+				}
+				
+				
+				
+				
+			}.padding()
+			
+			
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
